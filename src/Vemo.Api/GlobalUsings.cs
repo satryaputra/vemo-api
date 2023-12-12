ed131686846d3b539c9ce5a4b93181008f9c6a6c
satryaputra <muhammadsatrya12@gmail.com>
2023-12-12 14:33:59 +0700

    feat: add global usings file in Vemo.Api

---
+++ src/Vemo.Api/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using MediatR;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using Vemo.Application.Dtos;
+global using Vemo.Api.Common.Utils;
+global using Vemo.Application.Common.Exceptions;
